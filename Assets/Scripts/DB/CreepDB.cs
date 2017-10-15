006565dc97a9ff24f8d4a05d0fafa9885e1dcbd5
DiegoSourse <diego.2012.infognu@gmail.com>
2017-10-15 11:37:31 -0400

    Modificado a los personajes

--- Assets/Scripts/DB/CreepDB.cs
+++ Assets/Scripts/DB/CreepDB.cs
@@ -40,7 +40,7 @@ namespace TDTK {
 			private static GameObject CreatePrefab(){
 				GameObject obj=new GameObject();
 				obj.AddComponent<CreepDB>();
-				GameObject prefab=PrefabUtility.CreatePrefab("Assets/TDTK_Free/Resources/DB_TDTK/CreepDB.prefab", obj, ReplacePrefabOptions.ConnectToPrefab);
+				GameObject prefab=PrefabUtility.CreatePrefab("Assets/Resources/DB_TDTK/CreepDB.prefab", obj, ReplacePrefabOptions.ConnectToPrefab);
 				DestroyImmediate(obj);
 				AssetDatabase.Refresh ();
 				return prefab;
